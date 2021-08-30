af86a79cf6308391bad18137b8fb8bf972a928df
KaelHille <kael.hille@student.ap.be>
2021-08-30 23:37:10 +0200

    Making a new repo and Get-authHeader function

---
+++ more_powershell_for_github.ps1
@@ -0,0 +1,41 @@
+﻿function Get-authHeader{
+    param($Credential)
+
+$auth = [System.Convert]::ToBase64String([char[]]$c.GetNetworkCredential().Password)
+$headers = @{Authorization="Basic $auth"}
+
+return $headers
+
+}
+
+$username = 'KaelHille'
+
+Read-Host -AsSecureString -Prompt ’token’ |
+ConvertFrom-SecureString |
+Tee-Object .\secret.txt |
+ConvertTo-SecureString |
+Set-Variable ss_token
+$C = New-Object pscredential $username, $ss_token
+
+Set-GitHubAuthentication -SessionOnly `
+-Credential $C
+
+$repo = "MorePowerShellForGitHub"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
